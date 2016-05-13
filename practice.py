cc601680546e0ebdec6793f85954ef10b3c3dac9
MingCHsu <ming.hsu95@gmail.com>
2016-05-13 01:55:23 -0400

    Some code

--- practice.py
+++ practice.py
@@ -1,1 +1,10 @@
+from __future__ import print_function
+import cobra.test
+
 "not real code, just practice"
+
+model = cobra.test.create_test_model("textbook")
+print(len(model.reactions))
+atp = model.metabolites.get_by_id("atp_c")
+print (atp.name)
+print (atp.compartment)
